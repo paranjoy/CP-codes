054a12e937548ac948400486ea8d25001f6bee39
coder&coding <38404887+paranjoy@users.noreply.github.com>
2020-04-24 09:48:33 +0530

    DAY 23

---
+++ bitwise_and_of_number_range.cpp
@@ -0,0 +1,28 @@
+/*
+
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+Example 1:
+Input: [5,7]
+Output: 4
+
+Example 2:
+Input: [0,1]
+Output: 0
+*/
+//code snippet
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int count = 0;
+        
+        while(m != n){
+            m = m >> 1;
+            n = n >> 1;
+            count++;
+        }
+        
+        return (n << count);
+    }
+};
