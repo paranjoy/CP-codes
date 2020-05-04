9904810fab731b4c67df8d621de8dc70c0d65c68
coder&coding <38404887+paranjoy@users.noreply.github.com>
2020-05-04 15:05:53 +0530

    DAY 4

---
+++ Number_complement.cpp
@@ -0,0 +1,48 @@
+/*
+Given a positive integer, output its complement number.
+The complement strategy is to flip the bits of its binary representation.
+
+ 
+Example 1:
+
+Input: 5
+Output: 2
+
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+ 
+
+Example 2:
+
+Input: 1
+Output: 0
+
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+ 
+
+Note:
+
+The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+You could assume no leading zero bit in the integer’s binary representation.
+This question is the same as 1009: https://leetcode.com/problems/complement-of-base-10-integer/
+
+*/
+
+//code snippet
+class Solution {
+public:
+    int findComplement(int num) {
+        int res = 0;
+        int c = 0;
+        while(num > 0){
+            
+            int d = num & 1;
+            if(d == 1)
+                d = 0;
+            else
+                d = 1;
+            res += pow(2, c++) * d;
+            num = num >> 1;
+        }
+        return res;
+    }
+};
