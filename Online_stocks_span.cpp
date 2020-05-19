2b892cb1b33a0f95e72f0a24d1716014c99af38c
coder&coding <38404887+paranjoy@users.noreply.github.com>
2020-05-19 14:18:01 +0530

    DAY 19

---
+++ Online_stocks_span.cpp
@@ -0,0 +1,67 @@
+/*
+Write a class StockSpanner which collects daily price quotes for some stock, and returns the span of that stock's price 
+for the current day.
+
+The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going 
+backwards) for which the price of the stock was less than or equal to today's price.
+
+For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 60, 75, 85], then the stock spans wouldbe 
+[1, 1, 1, 2, 1, 4, 6].
+
+ 
+
+Example 1:
+Input: ["StockSpanner","next","next","next","next","next","next","next"], [[],[100],[80],[60],[70],[60],[75],[85]]
+Output: [null,1,1,1,2,1,4,6]
+Explanation: 
+First, S = StockSpanner() is initialized.  Then:
+S.next(100) is called and returns 1,
+S.next(80) is called and returns 1,
+S.next(60) is called and returns 1,
+S.next(70) is called and returns 2,
+S.next(60) is called and returns 1,
+S.next(75) is called and returns 4,
+S.next(85) is called and returns 6.
+
+Note that (for example) S.next(75) returned 4, because the last 4 prices
+(including today's price of 75) were less than or equal to today's price.
+ 
+
+Note:
+Calls to StockSpanner.next(int price) will have 1 <= price <= 10^5.
+There will be at most 10000 calls to StockSpanner.next per test case.
+There will be at most 150000 calls to StockSpanner.next across all test cases.
+The total time limit for this problem has been reduced by 75% for C++, and 50% for all other languages.
+*/
+//code snippet
+class StockSpanner {
+public:
+    
+    StockSpanner() {
+        counter = 0;
+        st.push({INT_MAX, 0});
+    }
+    
+    int next(int price) {
+        
+        counter++;
+        while(price >= st.top().first){
+            st.pop();
+        }
+        
+        int ans = counter - st.top().second;
+        
+        st.push({price, counter});
+        
+        return ans ;       
+    }
+private:
+    int counter; 
+    stack <pair<int, int>> st;
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
