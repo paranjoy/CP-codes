df7ea6c44f4c32edd9a6ff78d74c847005bde58b
paulparanjoy23 <38404887+paranjoy@users.noreply.github.com>
2020-05-27 11:54:06 +0530

    Create Gcd_of_two_numbers.cpp

---
+++ Gcd_of_two_numbers.cpp
@@ -0,0 +1,40 @@
+/*
+Take the following as input.
+
+A number (N1)
+A number (N2)
+Write a function which returns the GCD of N1 and N2. Print the value returned.
+
+Input Format
+Two integers seperated by a new line.
+
+Constraints
+0 < N1 < 1000000000
+0 < N2 < 1000000000
+
+Output Format
+Output a single integer which is the GCD of the given integers.
+
+Sample Input
+16 
+24
+Sample Output
+8
+Explanation
+The largest number that divides both N1 and N2 is called the GCD of N1 and N2.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b) { 
+    if (b == 0) 
+        return a; 
+    return gcd(b, a % b); 
+} 
+int main() {
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << gcd(a, b) << endl;
+	return 0;
+}
