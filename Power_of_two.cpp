49daf4021003ae521f263c236eb03b6e80575537
paulparanjoy23 <38404887+paranjoy@users.noreply.github.com>
2020-06-08 20:22:20 +0530

    DAY 8

---
+++ Power_of_two.cpp
@@ -0,0 +1,36 @@
+/*
+Given an integer, write a function to determine if it is a power of two.
+
+Example 1:
+Input: 1
+Output: true 
+Explanation: 2^0 = 1
+
+Example 2:
+Input: 16
+Output: true
+Explanation: 2^4 = 16
+
+Example 3:
+Input: 218
+Output: false
+*/
+
+//code snippet
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        if(n <= 0)
+            return false;       
+       
+        int  i = 0;
+        while(i < 32){
+            if(n == (1 << i))
+                return true;
+            i++;
+        }
+        
+        return false;
+    }
+};
