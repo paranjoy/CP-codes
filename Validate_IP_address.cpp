8a243eb9c62d23619384b913b4b709e9da7e9c27
paulparanjoy23 <38404887+paranjoy@users.noreply.github.com>
2020-06-17 11:47:00 +0530

    DAY 16

--- Validate_IP_address.cpp
+++ Validate_IP_address.cpp
@@ -35,7 +35,27 @@ Output: "Neither"
 Explanation: This is neither a IPv4 address nor a IPv6 address.
 */
 
-//code snippet
+//sol_1 :
+//code snippet with regex
+class Solution {
+public:
+    string validIPAddress(string IP) {
+        
+        regex v4("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){4}");
+        if(regex_match((IP + "."), v4))
+            return "IPv4";
+        
+        regex v6("((([0-9a-fA-F]){1,4})\\:){8}");
+        if(regex_match((IP + ":"), v6))
+            return "IPv6";
+        
+        return "Neither";
+    }
+};
+
+
+//sol_2 :
+//code snippet with brute attack
 class Solution {
 public:
     string validIPAddress(string IP) {
